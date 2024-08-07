83724db1eb23bc0b3d04a2a85a84ba3ab4ee2a2d
mrkormaks <mrkormaks@gmail.com>
2024-08-07 21:20:07 +0300

    Review changes

--- WorkingWithFiles/FileSumCalculator/src/FileSumCalculator.java
+++ WorkingWithFiles/FileSumCalculator/src/FileSumCalculator.java
@@ -1,25 +1,40 @@
 import java.io.*;
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class FileSumCalculator {
 
-  public static int calculateSum(int fileNum1, int fileNum2, String path) throws IOException, NumberFormatException {
+  private static final Logger logger = Logger.getLogger(FileSumCalculator.class.getName());
+
+  public static Integer calculateSum(int fileNum1, int fileNum2, String path) {
     int sum = 0;
     String[] filenames = {path + fileNum1 + ".txt", path + fileNum2 + ".txt"};
 
     for (String filename : filenames) {
+      int count = 0;
+
       try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
         String line;
-        int count = 0;
 
         while ((line = br.readLine()) != null) {
-          sum += Integer.parseInt(line);
-          count++;
+          try {
+            int number = Integer.parseInt(line);
+            sum += number;
+            count++;
+          } catch (NumberFormatException e) {
+            logger.warning("Файл " + filename + " содержит невалидные данные.");
+            return null;
+          }
         }
+      }
+      catch (IOException e) {
+        logger.warning("Не удалось открыть или прочитать файл " + filename);
+        return null;
+      }
 
-        if (count != 3) {
-          throw new IOException("Файл " + filename + " содержит отличное от 3-х число параметров.");
-        }
+      if (count != 3) {
+        logger.warning("Файл " + filename + " содержит отличное от 3-х число параметров.");
+        return null;
       }
     }
 
@@ -27,7 +42,7 @@ public class FileSumCalculator {
   }
 
   public static void main(String[] args) {
-    System.out.println("Введите два номера файлов от 1 до 10: ");
+    logger.info("Введите два номера файлов от 1 до 10: ");
     Scanner sc = new Scanner(System.in);
     int[] integers = new int[2];
 
@@ -35,13 +50,11 @@ public class FileSumCalculator {
       integers[i] = sc.nextInt();
     }
 
-    try {
-      int sum = calculateSum(integers[0], integers[1], "../TextFiles/");
-      System.out.println("Сумма чисел в файлах " + integers[0] + ".txt и " + integers[1] + ".txt : " + sum);
-    } catch (NumberFormatException e) {
-      System.out.println("Файлы содержат невалидные данные.");
-    } catch (IOException e) {
-      System.out.println(e.getMessage());
+    Integer sum = calculateSum(integers[0], integers[1], "../TextFiles/");
+    if (sum != null) {
+      logger.info("Сумма чисел в файлах " + integers[0] + ".txt и " + integers[1] + ".txt : " + sum);
+    } else {
+      logger.info("Произошла ошибка при вычислении суммы чисел в файлах.");
     }
   }
 }
--- WorkingWithFiles/FileSumCalculator/src/FileSumCalculatorTest.java
+++ WorkingWithFiles/FileSumCalculator/src/FileSumCalculatorTest.java
@@ -5,12 +5,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FileSumCalculatorTest {
   @Test
-  void testCalculateSumValidFiles() throws IOException {
+  void testCalculateSumValidFiles() {
     createTestFile("../TextFiles/1.txt", "10\n20\n30\n");
     createTestFile("../TextFiles/2.txt", "5\n15\n25\n");
 
-    int result = FileSumCalculator.calculateSum(1, 2, "../TextFiles/");
-    assertEquals(105, result);
+    Integer result = FileSumCalculator.calculateSum(1, 2, "../TextFiles/");
+    assertNotNull(result,  "Результат НЕ должен быть null для файлов с валидными данными");
+    assertEquals(105, result, "Ожидаемая сумма чисел в файлах 1.txt и 2.txt должна быть 105");
   }
 
   @Test
@@ -18,7 +19,8 @@ class FileSumCalculatorTest {
     createTestFile("../TextFiles/1.txt", "10\ninvalid\n30\n");
     createTestFile("../TextFiles/2.txt", "5\n15\n25\n");
 
-    assertThrows(NumberFormatException.class, () -> FileSumCalculator.calculateSum(1, 2, "../TextFiles/"));
+    Integer result = FileSumCalculator.calculateSum(1, 2, "../TextFiles/");
+    assertNull(result, "Результат должен быть null для файлов с невалидными данными");
   }
 
   private void createTestFile(String path, String content) {
