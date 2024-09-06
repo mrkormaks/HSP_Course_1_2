825c12ec01a5afe0c69b53773a5051f77d367f4b
mrkormaks <38488121+mrkormaks@users.noreply.github.com>
2024-09-07 00:07:17 +0300

    Update Level1.java

--- 28/Level1/Level1.java
+++ 28/Level1/Level1.java
@@ -1,16 +1,18 @@
+import java.util.*;
+
 public class Level1 {
-    public static int squirrel(int N) {
-        long factorial = 1;
-        while (N > 1) {
-            factorial *= N;
-            N--;
-        }
-
-        long result = factorial;
-        while (result > 9) {
-            result /= 10;
-        }
-
-        return (int)result;
+  public static int squirrel(int N) {
+    long factorial = 1;
+    while (N > 1) {
+      factorial *= N;
+      N--;
     }
+    
+    long result = factorial;
+    while (result > 9) {
+      result /= 10;
+    }
+
+    return (int)result;
+  }
 }
