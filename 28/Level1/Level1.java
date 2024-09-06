d578483990a9b4f771853aa0bec78f36dd94d45b
mrkormaks <mrkormaks@gmail.com>
2024-09-07 00:02:33 +0300

    28_Tasks_Ex_1

---
+++ 28/Level1/Level1.java
@@ -0,0 +1,16 @@
+public class Level1 {
+    public static int squirrel(int N) {
+        long factorial = 1;
+        while (N > 1) {
+            factorial *= N;
+            N--;
+        }
+
+        long result = factorial;
+        while (result > 9) {
+            result /= 10;
+        }
+
+        return (int)result;
+    }
+}
