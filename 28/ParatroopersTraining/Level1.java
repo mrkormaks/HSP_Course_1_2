0f673798b2c4bcd0c5767795dbf50b635c92e895
mrkormaks <mrkormaks@gmail.com>
2024-09-13 11:00:15 +0300

    28_Tasks_Ex_3_fix

--- 28/ParatroopersTraining/Level1.java
+++ 28/ParatroopersTraining/Level1.java
@@ -4,12 +4,20 @@ public class Level1 {
   public static int ConquestCampaign(int N, int M, int L, int[] battalion) {
     int days = 1;
     int count = 0;
+    int[] list_1 = new int[L];
+    int[] list_2 = new int[L];
     int[][] fieldsList = new int[N][M];
 
+    for (int c = 0; c < battalion.length; c++) {
+      if (c % 2 == 0) {
+        list_1[c / 2] = battalion[c];
+      } else {
+        list_2[c / 2] = battalion[c];
+      }
+    }
+
     for (int c = 0; c < L; c++) {
-      int row = battalion[2 * c] - 1;
-      int col = battalion[2 * c + 1] - 1;
-      fieldsList[row][col] = 1;
+      fieldsList[list_1[c] - 1][list_2[c] - 1] = 1;
     }
 
     for (int i = 0; i < N; i++) {
@@ -33,16 +41,16 @@ public class Level1 {
         for (int j = 0; j < M; j++) {
           if (fieldsList[i][j] >= 2) {
             if (i - 1 >= 0) {
-              fieldsList[i - 1][j] = 1;
+              fieldsList[i - 1][j]++;
             }
             if (i + 1 < N) {
-              fieldsList[i + 1][j] = 1;
+              fieldsList[i + 1][j]++;
             }
             if (j - 1 >= 0) {
-              fieldsList[i][j - 1] = 1;
+              fieldsList[i][j - 1]++;
             }
             if (j + 1 < M) {
-              fieldsList[i][j + 1] = 1;
+              fieldsList[i][j + 1]++;
             }
           }
         }
