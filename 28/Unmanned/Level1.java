9f25e6a8c59411331e5d5d2d40833a3c98aba346
mrkormaks <38488121+mrkormaks@users.noreply.github.com>
2024-10-21 11:09:30 +0300

    Update Level1.java

--- 28/Unmanned/Level1.java
+++ 28/Unmanned/Level1.java
@@ -1,24 +1,28 @@
-public static int Unmanned(int L, int N, int[][] track) {
-  int currentTime = 0;
-  int currentPosition = 0;
+import java.util.*;
 
-  for (int i = 0; i < N; i++) {
-    int[] trafficLight = track[i];
-    int position = trafficLight[0];
-    int redDuration = trafficLight[1];
-    int greenDuration = trafficLight[2];
-    int cycleDuration = redDuration + greenDuration;
+public class Level1 {
+  public static int Unmanned(int L, int N, int[][] track) {
+    int currentTime = 0;
+    int currentPosition = 0;
 
-    currentTime += position - currentPosition;
-    currentPosition = position;
+    for (int i = 0; i < N; i++) {
+      int[] trafficLight = track[i];
+      int position = trafficLight[0];
+      int redDuration = trafficLight[1];
+      int greenDuration = trafficLight[2];
+      int cycleDuration = redDuration + greenDuration;
 
-    int cycleTime = currentTime % cycleDuration;
-    if (cycleTime < redDuration) {
-      currentTime += redDuration - cycleTime;
+      currentTime += position - currentPosition;
+      currentPosition = position;
+
+      int cycleTime = currentTime % cycleDuration;
+      if (cycleTime < redDuration) {
+        currentTime += redDuration - cycleTime;
+      }
     }
+
+    currentTime += L - currentPosition;
+
+    return currentTime;
   }
-
-  currentTime += L - currentPosition;
-
-  return currentTime;
 }
