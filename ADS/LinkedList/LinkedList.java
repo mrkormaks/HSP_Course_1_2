751905f68674b30846d70756bcdd4a2d91754787
mrkormaks <mrkormaks@gmail.com>
2025-01-19 17:06:05 +0300

    ADS_#1_update

--- ADS/LinkedList/LinkedList.java
+++ ADS/LinkedList/LinkedList.java
@@ -27,36 +27,33 @@ public class LinkedList {
 
   public ArrayList<Node> findAll(int _value) {
     ArrayList<Node> nodes = new ArrayList<Node>();
-    Node current = this.head;
-    while (current != null) {
-      if (current.value == _value) {
-        nodes.add(current);
+    Node node = this.head;
+    while (node != null) {
+      if (node.value == _value) {
+        nodes.add(node);
       }
-      current = current.next;
+      node = node.next;
     }
     return nodes;
   }
 
   public boolean remove(int _value) {
-    if (this.head == null)
-      return false;
+    if (this.head == null) return false;
 
     if (this.head.value == _value) {
       this.head = this.head.next;
-      if (this.head == null) 
-        this.tail = null;
+      if (this.head == null) this.tail = null;
       return true;
     }
 
-    Node current = this.head;
-    while (current.next != null) {
-      if (current.next.value == _value) {
-        current.next = current.next.next;
-        if (current.next == null)
-          this.tail = current;
+    Node node = this.head;
+    while (node.next != null) {
+      if (node.next.value == _value) {
+        node.next = node.next.next;
+        if (node.next == null) this.tail = node;
         return true;
       }
-      current = current.next;
+      node = node.next;
     }
     return false;
   }
@@ -65,20 +62,21 @@ public class LinkedList {
     while (this.head != null && this.head.value == _value) {
       this.head = this.head.next;
     }
+
     if (this.head == null) {
       this.tail = null;
       return;
     }
 
-    Node current = this.head;
-    while (current.next != null) {
-      if (current.next.value == _value) {
-        current.next = current.next.next;
+    Node node = this.head;
+    while (node.next != null) {
+      if (node.next.value == _value) {
+        node.next = node.next.next;
       } else {
-        current = current.next;
+        node = node.next;
       }
     }
-    this.tail = current;
+    this.tail = node;
   }
 
   public void clear() {
@@ -88,10 +86,10 @@ public class LinkedList {
 
   public int count() {
     int count = 0;
-    Node current = this.head;
-    while (current != null) {
+    Node node = this.head;
+    while (node != null) {
       count++;
-      current = current.next;
+      node = node.next;
     }
     return count;
   }
@@ -100,8 +98,7 @@ public class LinkedList {
     if (_nodeAfter == null) {
       _nodeToInsert.next = this.head;
       this.head = _nodeToInsert;
-      if (this.tail == null)
-        this.tail = _nodeToInsert;
+      if (this.tail == null) this.tail = _nodeToInsert;
       return;
     }
 
@@ -111,85 +108,6 @@ public class LinkedList {
     if (_nodeAfter == this.tail) {
       this.tail = _nodeToInsert;
     }
-  }
-
-  public static LinkedList sumLists(LinkedList list1, LinkedList list2) {
-    if (list1.count() != list2.count()) {
-      return null;
-    }
-
-    LinkedList result = new LinkedList();
-    Node current1 = list1.head;
-    Node current2 = list2.head;
-
-    while (current1 != null) {
-      result.addInTail(new Node(current1.value + current2.value));
-      current1 = current1.next;
-      current2 = current2.next;
-    }
-
-    return result;
-  }
-
-  public void testRemove() {
-    LinkedList list = new LinkedList();
-    assert !list.remove(1);
-    assert list.head == null;
-    assert list.tail == null;
-
-    list.addInTail(new Node(1));
-    assert list.remove(1);
-    assert list.head == null;
-    assert list.tail == null;
-
-    list.addInTail(new Node(1));
-    list.addInTail(new Node(2));
-    list.addInTail(new Node(3));
-    assert list.remove(2);
-    assert list.count() == 2;
-  }
-
-  public void testRemoveAll() {
-    LinkedList list = new LinkedList();
-    list.removeAll(1);
-    assert list.head == null;
-    assert list.tail == null;
-
-    list.addInTail(new Node(1));
-    list.addInTail(new Node(1));
-    list.addInTail(new Node(2));
-    list.removeAll(1);
-    assert list.count() == 1;
-    assert list.head.value == 2;
-    assert list.tail.value == 2;
-  }
-
-  public void testInsertAfter() {
-    LinkedList list = new LinkedList();
-    Node node1 = new Node(1);
-    list.insertAfter(null, node1);
-    assert list.head == node1;
-    assert list.tail == node1;
-
-    Node node2 = new Node(2);
-    list.insertAfter(node1, node2);
-    assert list.head.next == node2;
-    assert list.tail == node2;
-  }
-
-  public void testSumLists() {
-    LinkedList list1 = new LinkedList();
-    LinkedList list2 = new LinkedList();
-
-    list1.addInTail(new Node(1));
-    list1.addInTail(new Node(2));
-    list2.addInTail(new Node(3));
-    list2.addInTail(new Node(4));
-
-    LinkedList result = sumLists(list1, list2);
-    assert result.count() == 2;
-    assert result.head.value == 4;
-    assert result.tail.value == 6;
   }
 }
 
