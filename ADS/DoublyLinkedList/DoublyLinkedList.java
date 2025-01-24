a17fc37b8277d9865b994d45a9358514064849c0
mrkormaks <mrkormaks@gmail.com>
2025-01-24 22:06:32 +0300

    ADS_#2

---
+++ ADS/DoublyLinkedList/DoublyLinkedList.java
@@ -0,0 +1,137 @@
+public class LinkedList2 {
+  public Node head;
+  public Node tail;
+
+  public LinkedList2() {
+    head = null;
+    tail = null;
+  }
+
+  public void addInTail(Node _item) {
+    if (head == null) {
+      this.head = _item;
+      this.head.next = null;
+      this.head.prev = null;
+    } else {
+      this.tail.next = _item;
+      _item.prev = tail;
+    }
+    this.tail = _item;
+  }
+
+  public Node find(int _value) {
+    Node current = head;
+    while (current != null) {
+      if (current.value == _value) {
+        return current;
+      }
+      current = current.next;
+    }
+    return null;
+  }
+
+  public ArrayList<Node> findAll(int _value) {
+    ArrayList<Node> nodes = new ArrayList<>();
+    Node current = head;
+    while (current != null) {
+      if (current.value == _value) {
+        nodes.add(current);
+      }
+      current = current.next;
+    }
+    return nodes;
+  }
+
+  public boolean remove(int _value) {
+    Node current = head;
+
+    while (current != null) {
+      if (current.value == _value) {
+        if (current.prev != null) {
+          current.prev.next = current.next;
+        } else {
+          head = current.next;
+        }
+
+        if (current.next != null) {
+          current.next.prev = current.prev;
+        } else {
+          tail = current.prev;
+        }
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
+
+  public void removeAll(int _value) {
+    Node current = head;
+
+    while (current != null) {
+      Node next = current.next;
+      if (current.value == _value) {
+        if (current.prev != null) {
+          current.prev.next = current.next;
+        } else {
+          head = current.next;
+        }
+
+        if (current.next != null) {
+          current.next.prev = current.prev;
+        } else {
+          tail = current.prev;
+        }
+      }
+      current = next;
+    }
+  }
+
+  public void clear() {
+    head = null;
+    tail = null;
+  }
+
+  public int count() {
+    int count = 0;
+    for (Node current = head; current != null; current = current.next) {
+      count++;
+    }
+    return count;
+  }
+
+  public void insertAfter(Node _nodeAfter, Node _nodeToInsert) {
+    if (_nodeAfter == null) {
+      _nodeToInsert.next = head;
+      if (head != null) {
+        head.prev = _nodeToInsert;
+      }
+      head = _nodeToInsert;
+      if (tail == null) {
+        tail = _nodeToInsert;
+      }
+    } else {
+      _nodeToInsert.next = _nodeAfter.next;
+      _nodeToInsert.prev = _nodeAfter;
+      if (_nodeAfter.next != null) {
+        _nodeAfter.next.prev = _nodeToInsert;
+      }
+      _nodeAfter.next = _nodeToInsert;
+      if (_nodeAfter == tail) {
+        tail = _nodeToInsert;
+      }
+    }
+  }
+}
+
+class Node {
+  public int value;
+  public Node next;
+  public Node prev;
+
+  public Node(int _value) {
+    value = _value;
+    next = null;
+    prev = null;
+  }
+}
